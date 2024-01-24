1edafd8febff67cf262c5ee6fc9d08d31c1f5b0c
ayushregmi85 <ayushregmi85@gmail.com>
2024-01-24 14:05:35 +0545

    validate number of pizzas

--- Task1/Task1.py
+++ Task1/Task1.py
@@ -5,3 +5,8 @@ def calculate_total_price(is_tuesday, num_pizzas, is_delivery, is_app_order):
     # Apply Tuesday discount
     if is_tuesday:
         pizza_price *= 0.5
+
+    # Validate number of pizzas
+    if num_pizzas < 0:
+        print("Please enter a positive integer!")
+        return None
