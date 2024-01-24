4685fa7e3b782999f5232061a2124c159b3024fd
ayushregmi85 <ayushregmi85@gmail.com>
2024-01-24 14:16:42 +0545

    log analysis function

---
+++ Task2/Task2.py
@@ -0,0 +1,39 @@
+import sys
+
+# Commit: Log Analysis Function
+def analyze_log(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
+
+        cat_visits = 0
+        intruder_doused = 0
+        total_time_in_house = 0
+        longest_visit = 0
+        shortest_visit = float('inf')
+
+        for line in lines:
+            if line.strip() == 'END':
+                break
+
+            parts = line.strip().split(',')
+            cat_name, entry_time, exit_time = parts
+
+            entry_time = int(entry_time)
+            exit_time = int(exit_time)
+
+            if cat_name == 'OURS':
+                cat_visits += 1
+                total_time_in_house += exit_time - entry_time
+
+                visit_duration = exit_time - entry_time
+                longest_visit = max(longest_visit, visit_duration)
+                shortest_visit = min(shortest_visit, visit_duration)
+
+            elif cat_name == 'THEIRS':
+                intruder_doused += 1
+
+        if cat_visits == 0:
+            avg_visit_length = 0
+        else:
+            avg_visit_length = total_time_in_house // cat_visits
