2ce780386d9f91ea106d9866cca374375e5df092
ayushregmi85 <ayushregmi85@gmail.com>
2024-01-24 14:04:52 +0545

    apply tuesday discount

--- Task1/Task1.py
+++ Task1/Task1.py
@@ -1,3 +1,7 @@
 def calculate_total_price(is_tuesday, num_pizzas, is_delivery, is_app_order):
     pizza_price = 12
     delivery_cost = 2.5
+
+    # Apply Tuesday discount
+    if is_tuesday:
+        pizza_price *= 0.5
