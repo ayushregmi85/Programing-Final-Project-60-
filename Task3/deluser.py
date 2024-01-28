41f9d089eb03f2e4a56deb71f417de131e5c47b2
ayushregmi85 <ayushregmi85@gmail.com>
2024-01-28 12:15:36 +0545

    If not, write the line back to the file

--- Task3/deluser.py
+++ Task3/deluser.py
@@ -10,3 +10,5 @@ def delete_user(username, password_file):
         for line in lines:
             # Check if the username in the current line matches the one to be deleted
             if line.split(':')[0] != username:
+                # If not, write the line back to the file
+                file.write(line)
