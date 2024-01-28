f38153813a662861ea886d582a4dcae48d963172
ayushregmi85 <ayushregmi85@gmail.com>
2024-01-28 12:13:56 +0545

    Open the password file in 'write' mode

--- Task3/deluser.py
+++ Task3/deluser.py
@@ -2,3 +2,7 @@ def delete_user(username, password_file):
     # Read all lines from the password file
     with open(password_file, 'r') as file:
         lines = file.readlines()
+
+    # Open the password file in 'write' mode
+    with open(password_file, 'w') as file:
+        user_deleted = False
